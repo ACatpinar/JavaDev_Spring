0752175bde8b3d7cea8e7d2e189251c43e45da40
ACatpinar <acatpinar@windowslive.com>
2020-11-17 21:50:57 -0500

    TigthlyCoupled

--- Spring-00-TightlyCoupled/src/Application.java
+++ Spring-00-TightlyCoupled/src/Application.java
@@ -4,10 +4,14 @@ public class Application {
         FullTimeMentor fullTime = new FullTimeMentor();
         PartTimeMentor partTime = new PartTimeMentor();
 
-        Mentor mentor = new Mentor(fullTime,partTime); // mentor object has dependency to the part and full time objects
+        Mentor mentor = new Mentor(fullTime,partTime);
         mentor.manageAccount();
-
-        // when we want to add new Mentor, we need to add lots of mentor obj to the other classes too.
-        // using tightlyCoupled is not useful.
+        /*
+        mentor object has dependency to the part and full time objects
+        when we want to add new Mentor, we need to add lots of mentor obj to the other classes too.
+        using tightlyCoupled is not useful.
+        Coupling means usage of an object by different object,
+        basically means an object needs another obj to complete assigned task
+         */
     }
 }

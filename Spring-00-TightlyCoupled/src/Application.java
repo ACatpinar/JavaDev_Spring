8150dac9be87b2212bd29efbcd2be1a147c8a857
ACatpinar <acatpinar@windowslive.com>
2020-11-17 21:48:38 -0500

    TigthlyCoupled

--- Spring-00-TightlyCoupled/src/Application.java
+++ Spring-00-TightlyCoupled/src/Application.java
@@ -3,7 +3,11 @@ public class Application {
     public static void main(String[] args) {
         FullTimeMentor fullTime = new FullTimeMentor();
         PartTimeMentor partTime = new PartTimeMentor();
-        Mentor mentor = new Mentor(fullTime,partTime);
+
+        Mentor mentor = new Mentor(fullTime,partTime); // mentor object has dependency to the part and full time objects
         mentor.manageAccount();
+
+        // when we want to add new Mentor, we need to add lots of mentor obj to the other classes too.
+        // using tightlyCoupled is not useful.
     }
 }

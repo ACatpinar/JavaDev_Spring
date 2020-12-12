2b541a0d608b425a77e607d75b84df17119dc176
ACatpinar <acatpinar@windowslive.com>
2020-12-12 11:35:49 -0500

    JavaCode-Configuration

---
+++ Spring-06-JavaCode-Configuration/src/main/java/com/services/java.java
@@ -0,0 +1,10 @@
+package com.services;
+
+import com.interfaces.Course;
+
+public class java implements Course {
+
+    public void getTeachingHours() {
+        System.out.println("Weekly Teaching Hours 30 ");
+    }
+}

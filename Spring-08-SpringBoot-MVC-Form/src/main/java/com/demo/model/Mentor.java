c1aa63d668fdf4ddd837c3872befbac257ae3b03
acatpinar <acatpinar@windowslive.com>
2021-01-17 14:51:26 -0500

    MVC-Form

---
+++ Spring-08-SpringBoot-MVC-Form/src/main/java/com/demo/model/Mentor.java
@@ -0,0 +1,20 @@
+package com.demo.model;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Mentor {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String gender;
+    private String graduated;
+    private String batch;
+    private String company;
+
+
+}
